0fa3ecde2e09f675f9ab7ac4c94cb5bb1a6d8543
apavlushina <ssemina7@gmail.com>
2020-01-13 10:34:46 +0100

    creating db

--- products/model.js
+++ products/model.js
@@ -18,27 +18,27 @@ const Museum = db.define("museum", {
 
 Museum.sync()
   .then(() => console.log("Model synchronization completes"))
-  // .then(async () => {
-  //   console.log("create raws", initState[1]);
-  //   for (let i = 0; i < initState.length; i++) {
-  //     const coord = await request(
-  //       `https://maps.googleapis.com/maps/api/geocode/json?address=${initState[i].SubTitle}+${initState[i].Title}&key=${myKey}`
-  //     ).then(response => response.body.results[0].geometry.location);
-  //     console.log("coord", coord);
-  //     const lat = coord.lat;
-  //     console.log("lat", lat);
-  //     const lng = coord.lng;
-  //     console.log("lng", lng);
-  //     await Museum.create({
-  //       id: initState[i].MuseumId,
-  //       title: initState[i].SubTitle,
-  //       city: initState[i].Title,
-  //       url: initState[i].ImageUrl,
-  //       lat: lat,
-  //       lng: lng
-  //     });
-  //   }
-  // })
+  .then(async () => {
+    console.log("create raws", initState[1]);
+    for (let i = 0; i < initState.length; i++) {
+      const coord = await request(
+        `https://maps.googleapis.com/maps/api/geocode/json?address=${initState[i].SubTitle}+${initState[i].Title}&key=${myKey}`
+      ).then(response => response.body.results[0].geometry.location);
+      console.log("coord", coord);
+      const lat = coord.lat;
+      console.log("lat", lat);
+      const lng = coord.lng;
+      console.log("lng", lng);
+      await Museum.create({
+        id: initState[i].MuseumId,
+        title: initState[i].SubTitle,
+        city: initState[i].Title,
+        url: initState[i].ImageUrl,
+        lat: lat,
+        lng: lng
+      });
+    }
+  })
   .catch(console.error);
 
 module.exports = Museum;
